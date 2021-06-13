b4e7c509ecc13ee20a842563779bf802bfb855b6
innoc <apthomlinson@gmail.com>
2021-06-12 19:39:25 -0700

    overlay components

---
+++ src/components/Page.js
@@ -0,0 +1,14 @@
+import StyleOverlay from "./StyleOverlay";
+import BackgroundShapes from "./BackgroundShapes";
+
+function Page() {
+  return (
+    <div>
+        <BackgroundShapes />
+
+        <StyleOverlay />
+    </div>
+  );
+}
+
+export default Page;
