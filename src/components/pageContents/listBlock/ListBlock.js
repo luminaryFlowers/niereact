81bbabd3c20a25c61a62fd82d5c33cd3a944841e
innoc <apthomlinson@gmail.com>
2021-06-20 11:57:06 -0700

    decorator beneath navbar and page now extends across the screen & shows in mobile

--- src/components/pageContents/listBlock/ListBlock.js
+++ src/components/pageContents/listBlock/ListBlock.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../../../css/page-contents-list-block.css';
 import ListFlourish from "./ListFlourish";
+import ListBlockOption from "./ListBlockOption";
 
 function ListBlock() {
     return (
         <React.Fragment>
             <ListFlourish />
 
-            <div style={{overflowY: "auto", height: "calc(100% - 46px)"}}>
-                <p className="content-block-text">
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
+            <div id="content-block-list-contents">
+                <ListBlockOption />
             </div>
 
             <ListFlourish />
---
+++ src/components/pageContents/listBlock/ListBlockOption.js
@@ -0,0 +1,10 @@
+function ListBlockOption() {
+    return (
+        <div className="list-block-option">
+            <i className={" list-block-option-icon"} />
+            <p>hi</p>
+        </div>
+    );
+}
+
+export default ListBlockOption;
