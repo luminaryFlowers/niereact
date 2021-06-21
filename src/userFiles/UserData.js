29076ab767b9aa3d5ff098a39524f593431ea3d4
tortoisetea <athomlinson@my.bcit.ca>
2021-06-20 18:29:51 -0700

    small adjustments to styling, beginning to work on the data for the sample

--- src/userFiles/UserData.js
+++ src/userFiles/UserData.js
@@ -9,7 +9,51 @@
 let UserData = {
     title: "NieReact",
     pageData: {
+		
         "Home": {
+            icon: "bi-house-fill",
+            format: "text",
+            tabs: {
+                "NieReact": {
+                    icon: "bi-house-fill",
+                    left: {
+                        header: "Welcome",
+                        text: "This is a sample website for NieReact! This website contains a guide on how to get your own NieR:Automata themed website up and running."
+                    },
+                    right: {
+                        header: "Sample Image",
+                        text: "idk what to put here lol",
+                        image: "placeholder-klaus.jpg"
+                    }
+                },
+				
+                "What is NieReact?": {
+                    icon: "bi-question-circle-fill",
+                    left: {
+                        header: "What is it?",
+                        text: "NieReact is a NieR:Automata themed website template that was created using React. "
+                    },
+                    right: {
+                        header: "Why is it?",
+                        text: "I made a website just like this in December of 2020, but I didn't really bother making the code good at all. I wanted to make it better, and figured I'd do it in React as a learning experience sort of deal."
+                    }
+                },
+				
+				"Features": {
+					icon: "bi-book-fill",
+                    left: {
+					    header: "hi",
+                        text: "hi"
+                    },
+                    right: {
+					    header: "hi",
+                        text: "hello"
+                    }
+				}
+            }
+        },
+
+        "Prereqs": {
             icon: "bi-house-fill",
             format: "text",
             tabs: {
@@ -17,29 +61,57 @@ let UserData = {
                     icon: "bi-archive",
                     left: {
                         header: "left header",
-                        text: "left textt",
-                        image: "placeholder-klaus.jpg"
+                        text: "left texte"
                     },
                     right: {
                         header: "right header",
-                        text: "right textttt"
+                        text: "right texte"
                     }
                 },
                 "Other One": {
                     icon: "bi-archive",
                     left: {
                         header: "left header",
-                        text: "left textm"
+                        text: "left textr"
                     },
                     right: {
                         header: "right header",
-                        text: "right textm"
+                        text: "right textr"
                     }
-                }
+                },
             }
         },
-
-        "テスト": {
+		
+		"Tutorial": {
+            icon: "bi-house-fill",
+            format: "text",
+            tabs: {
+                "Test": {
+                    icon: "bi-archive",
+                    left: {
+                        header: "left header",
+                        text: "left texte"
+                    },
+                    right: {
+                        header: "right header",
+                        text: "right texte"
+                    }
+                },
+                "Other One": {
+                    icon: "bi-archive",
+                    left: {
+                        header: "left header",
+                        text: "left textr"
+                    },
+                    right: {
+                        header: "right header",
+                        text: "right textr"
+                    }
+                },
+            }
+        },
+		
+		"Credits": {
             icon: "bi-house-fill",
             format: "text",
             tabs: {
@@ -67,6 +139,7 @@ let UserData = {
                 },
             }
         }
+		
     }
 }
 
